64de8a4a77ea0f1acca586e4f281a1fc3c910c9e
Heron <darkgezar@gmail.com>
2021-11-08 20:28:06 +0200

    Player.cs add move to axis x

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private float movementSpeed = 10f;
+    private float _directionMove;
+    private Rigidbody2D _playerRB;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _playerRB = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
+        _directionMove = Input.GetAxisRaw("Horizontal") * movementSpeed;
         
+    }
+    private void FixedUpdate()
+    {
+        Vector2 velosity = _playerRB.velocity;
+        velosity.x = _directionMove;
+        _playerRB.velocity = velosity;
     }
 }
